40b044a0a9d9671c0854ce490f3f30ff665ed57a
Lionel DURAND <lioneldurand@gmail.com>
2023-09-09 12:40:57 +0200

    Test controller + add coverage tooling

--- test/controllers/api/v1/wines_controller_test.rb
+++ test/controllers/api/v1/wines_controller_test.rb
@@ -4,4 +4,17 @@ class Api::V1::WinesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get all wines" do
+    get api_v1_wines_url
+    assert_response :success
+    assert_equal 2, response.parsed_body.length
+  end
+
+  test "should get a wine by id" do
+    get api_v1_wine_url(wines(:one).id)
+    assert_equal "Château 1", response.parsed_body["name"]
+    assert_response :success
+  end
+
 end

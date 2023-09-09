8e556b062065a94cd4d428188d2836674c5a0aa7
Lionel DURAND <lioneldurand@gmail.com>
2023-09-09 12:09:19 +0200

    Generate model and controller with rails

---
+++ db/migrate/20230909100806_create_wines.rb
@@ -0,0 +1,12 @@
+class CreateWines < ActiveRecord::Migration[7.0]
+  def change
+    create_table :wines do |t|
+      t.string :name
+      t.float :price
+      t.string :store_url
+      t.float :note
+
+      t.timestamps
+    end
+  end
+end
